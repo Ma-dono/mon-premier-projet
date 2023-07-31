ef3d8e21d713accad205d7bf1b1a4e23bb60d106
Madono <marienoelliekonan912@gmail.com>
2023-07-31 16:30:06 +0000

    banner homepage first

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+function recalcultotal(){
+    var  input = this.parentNode.querySelector('input[type=number]');
+}
 
+function btnadd(){
+    var  input =  this.parentNode.querySelector('input[type=number]');
+    input.stepUp();
+}
+function btnminus(){
+    var  input =  this.parentNode.querySelector('input[type=number]');
+    input.stepDown();
+}
